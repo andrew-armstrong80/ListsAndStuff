768e2860b5c321303b1499d5b22d26d063ee81b8
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-01-30 10:43:40 +0000

    Lists initial

---
+++ src/main/scala/Lists/Descender.scala
@@ -0,0 +1,14 @@
+package Lists
+
+
+
+object Descender extends App {
+
+  val x = List.range(1, num + 1).reverse
+
+
+  def num = 5
+
+  println(x)
+
+}
---
+++ src/main/scala/Lists/Repeater.scala
@@ -0,0 +1,9 @@
+package Lists
+
+
+
+object Repeater extends App {
+
+  def num = 5
+  println (s"Hello" * num)
+}
